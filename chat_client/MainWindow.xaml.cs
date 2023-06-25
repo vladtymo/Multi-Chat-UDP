a5fcf0776bb1c212cad17c18f2f184b4f5700619
vladtymo <tymo.vlad@gmail.com>
2023-06-25 14:12:57 +0300

    Add project files.

---
+++ chat_client/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace chat_client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UdpClient client = new();
+        private bool isListening = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Listen()
+        {
+            while (isListening)
+            {
+                var result = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                msgList.Items.Add(message);
+            }
+        }
+
+        private void SendBtnClick(object sender, RoutedEventArgs e)
+        {
+            string text = msgTxtBox.Text;
+            SendMessage(text);
+        }
+
+        private void JoinBtnClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<join>");
+            isListening = true;
+            Listen();
+        }
+
+        private void LeaveBtnClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<leave>");
+            isListening = false;
+        }
+
+        private void SendMessage(string text)
+        {
+            IPEndPoint serverIp = new(IPAddress.Parse(ipTxtBox.Text), int.Parse(portTxtBox.Text));
+
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            client.Send(data, serverIp);
+        }
+    }
+}
---
+++ chat_server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// ip: 127.0.0.1
+const int port = 3737;
+const string JOIN_CMD = "<join>";
+const string LEAVE_CMD = "<leave>";
+
+UdpClient server = new(port);
+// HashSet<> can not contains duplicates
+HashSet<IPEndPoint> members = new();
+
+while (true)
+{
+    IPEndPoint clientIp = null;
+    byte[] data = server.Receive(ref clientIp);
+
+    string message = Encoding.UTF8.GetString(data);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {message} from {clientIp}");
+
+    switch (message)
+    {
+        case JOIN_CMD:
+            members.Add(clientIp);
+            break;
+        case LEAVE_CMD:
+            members.Remove(clientIp);
+            break;
+        default:
+            foreach (var m in members)
+                server.Send(data, m);
+            break;
+    }
+}
